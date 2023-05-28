20a23f85f459e41c6c3de6a1516f0df88565c1b1
Sahil Suman <sahilsuman933@gmail.com>
2023-05-29 00:51:38 +0530

    Project initialization
    Signed-off-by: Sahil Suman <sahilsuman933@gmail.com>

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <p>Hello Sahil!</p>
+    </>
+  );
+};
+
+export default Home;
